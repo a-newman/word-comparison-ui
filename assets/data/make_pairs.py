709acd850fc3f1f074a7e0d481c53dca6312d67e
Anelise Newman <apnewman@mit.edu>
2018-02-09 17:51:35 -0500

    fix task allocation bug

--- assets/data/make_pairs.py
+++ assets/data/make_pairs.py
@@ -29,11 +29,10 @@ words = [
     'marked'
 ]
 
-pairs = []
-
 wordsset = set(words)
 assert len(wordsset) == len(words)
 
+pairs = []
 for i in range(len(words)): 
     for j in range(i+1, len(words)): 
         pair = [words[i], words[j]]
@@ -41,29 +40,63 @@ for i in range(len(words)):
 
 print(len(pairs))
 
-with open('pairs.json', 'w') as outfile: 
-    json.dump(pairs, outfile)
 
-random.shuffle(pairs)
+def generate(): 
 
-# divide the pairs into the right groups.
-num_groups = math.ceil(len(pairs)/TASK_SIZE)
-print("num groups", num_groups)
+    with open('pairs.json', 'w') as outfile: 
+        json.dump(pairs, outfile)
 
-num_repeats = num_groups*TASK_SIZE - len(pairs)
-print("num repeats", num_repeats)
+    random.shuffle(pairs)
 
-repeats = random.sample(pairs, num_repeats)
+    # divide the pairs into the right groups.
+    num_groups = math.ceil(len(pairs)/TASK_SIZE)
+    print("num groups", num_groups)
 
-pairs.extend(repeats)
+    num_repeats = num_groups*TASK_SIZE - len(pairs)
+    print("num repeats", num_repeats)
 
-assert len(pairs) == num_groups*TASK_SIZE
+    repeats = random.sample(pairs, num_repeats)
+    with open('repeats.json', 'w') as outfile: 
+        json.dump(repeats, outfile)
 
-# now we can divide up these things 
+    pairs.extend(repeats)
 
-for i in range(0, len(pairs) - TASK_SIZE, TASK_SIZE): 
-    group_num = i/TASK_SIZE + 1
-    group = pairs[i:i+TASK_SIZE]
-    assert len(group) == TASK_SIZE
-    with open(str(int(group_num)) + '.json', 'w') as outfile: 
-        json.dump(group, outfile)
+    assert len(pairs) == num_groups*TASK_SIZE
+
+    # now we can divide up these things 
+
+    for i in range(0, len(pairs), TASK_SIZE): 
+        group_num = i/TASK_SIZE + 1
+        group = pairs[i:i+TASK_SIZE]
+        assert len(group) == TASK_SIZE
+        with open(str(int(group_num)) + '.json', 'w') as outfile: 
+            json.dump(group, outfile)
+
+def check(): 
+    def myhash(a): 
+        h = str(sorted(a))
+        #print(h)
+        return h
+
+    file_pairs = set()
+    calculated_pairs = set()
+
+    for elt in pairs: 
+        calculated_pairs.add(myhash(elt))
+
+    for i in range(1, 10): 
+        print(i)
+        with open(str(int(i)) + '.json', 'r') as infile: 
+            this_file_pairs = json.load(infile)
+            print(len(this_file_pairs))
+            for elt in this_file_pairs: 
+                if myhash(elt) in file_pairs: 
+                    print("REPEAT ELEMENT:", elt)
+                file_pairs.add(myhash(elt))
+
+    print("len file_pairs: ", len(file_pairs))
+    print("len calculated_pairs: ", len(calculated_pairs))
+
+
+if __name__ == '__main__': 
+    generate()
